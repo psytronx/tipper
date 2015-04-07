34a2f3dc1c1b51d5ce375d7cb6a3b6f149cad2f0
psytronx <roger.c.hom@gmail.com>
2015-04-07 16:44:29 -0700

    Basic functionality for calculating tip complete, per https://vimeo.com/102084767.

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipValueLabel: UILabel!
+    @IBOutlet weak var totalValueLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    let tipPercentages = [0.15, 0.18, 0.20]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipValueLabel.text = "$0.00"
+        totalValueLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var billAmount = (billField.text as NSString).doubleValue
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipValueLabel.text = String(format: "$%.2f", tip)
+        totalValueLabel.text = String(format: "$%.2f", total)
+        
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
